50f4f89ec13b2c4720ace86ddaf7c75e172e5473
iwasa-isfactory <iwasa@isfactory.co.jp>
2017-11-10 18:27:53 +0900

    素因数算出プログラムの例外処理を改修。

--- pipeline/example1_1.py
+++ pipeline/example1_1.py
@@ -7,16 +7,20 @@ def factorize(n):
     example of a CPU bound task
     """
     n = int(n)
-    if n == 1: return 1, []
+    if n < 2: raise ValueError('Invalid number for factorization', n)
     found = []
     for i in range(2, (n//2)+1):
         if n%i == 0:
             if all(i % j != 0 for j in found): found.append(i)
-
     if not found: return n, [n]
     return n, found
 
 if __name__ == '__main__':
     with futures.ProcessPoolExecutor() as pool:
-        for num, factors in pool.map(factorize, range(1, 10001)):
-            print('{}: {}'.format(num, factors))
+        fs = [pool.submit(factorize, x) for x in range(1, 10001)]
+        for f in futures.as_completed(fs):
+            try:
+                number, factors = f.result()
+            except ValueError:
+                continue
+            print('{}: {}'.format(number, factors))
