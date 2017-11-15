f2eeaa7d35097c17df171ebf425c559475fa5ce4
daring-board <mahotox101500@gmail.com>
2017-11-16 04:10:15 +0900

    結果をコミット

--- pred_fx/check_correct.py
+++ pred_fx/check_correct.py
@@ -8,8 +8,7 @@ if __name__=='__main__':
         actual_list = [float(line.split(',')[1]) for line in lines]
         pred_list = [float(line.split(',')[2]) for line in lines]
         day_length_list = [1, 2, 3, 4, 5, 6, 11, 16, 21, 41, 61]
-        corrects_list = []
-        prediction_list = []
+        line = ''
         for l in day_length_list:
             corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] > 0.02 else 0 for idx in range(len(actual_list))]
             prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] > 0.02 else 0 for idx in range(len(pred_list))]
@@ -18,7 +17,7 @@ if __name__=='__main__':
                 ret = 1 if corrects[idx]==prediction[idx] else 0
                 #print('%d, %d, %d'%(corrects[idx], prediction[idx], ret))
                 if ret == 1: count += 1
-            print('高騰: %dd: 正答数: %d, 正答率: %.2f'%(l, count, 100*count/len(actual_list)))
+            line += '高騰2％: %dd: 正答数: %d, 正答率: %.2f (％)\n'%(l, count, 100*count/len(actual_list))
             corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] < -0.02 else 0 for idx in range(len(actual_list))]
             prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] < -0.02 else 0 for idx in range(len(pred_list))]
             count = 0
@@ -26,4 +25,5 @@ if __name__=='__main__':
                 ret = 1 if corrects[idx]==prediction[idx] else 0
                 #print('%d, %d, %d'%(corrects[idx], prediction[idx], ret))
                 if ret == 1: count += 1
-            print('下落: %dd: 正答数: %d, 正答率: %.2f'%(l, count, 100*count/len(actual_list)))
+            line += '下落2％: %dd: 正答数: %d, 正答率: %.2f (％)\n'%(l, count, 100*count/len(actual_list))
+        with open('%s/probs.txt'%dir_path, 'w') as f: f.write(line)
