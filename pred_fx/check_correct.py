5056c188b508af728287eeb2fec29163fccb3f6d
daring-board <mahotox101500@gmail.com>
2017-11-16 04:25:28 +0900

    commit

--- pred_fx/check_correct.py
+++ pred_fx/check_correct.py
@@ -8,22 +8,22 @@ if __name__=='__main__':
         actual_list = [float(line.split(',')[1]) for line in lines]
         pred_list = [float(line.split(',')[2]) for line in lines]
         day_length_list = [1, 2, 3, 4, 5, 6, 11, 16, 21, 41, 61]
-        line = ''
+        rate = 0.02
+        line = '正解, 日数, 高騰正答数, 下落正答数, 高騰正答率(％), 下落正答率(％)\n'
         for l in day_length_list:
-            corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] > 0.02 else 0 for idx in range(len(actual_list))]
-            prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] > 0.02 else 0 for idx in range(len(pred_list))]
-            count = 0
+            corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] > rate else 0 for idx in range(len(actual_list))]
+            prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] > rate else 0 for idx in range(len(pred_list))]
+            count_raise = 0
             for idx in range(len(actual_list)):
                 ret = 1 if corrects[idx]==prediction[idx] else 0
                 #print('%d, %d, %d'%(corrects[idx], prediction[idx], ret))
-                if ret == 1: count += 1
-            line += '高騰2％: %dd: 正答数: %d, 正答率: %.2f (％)\n'%(l, count, 100*count/len(actual_list))
-            corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] < -0.02 else 0 for idx in range(len(actual_list))]
-            prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] < -0.02 else 0 for idx in range(len(pred_list))]
-            count = 0
+                if ret == 1: count_raise += 1
+            corrects = [1 if (actual_list[idx]-actual_list[idx-l])/actual_list[idx] < -rate else 0 for idx in range(len(actual_list))]
+            prediction = [1 if (pred_list[idx]-pred_list[idx-l])/pred_list[idx] < -rate else 0 for idx in range(len(pred_list))]
+            count_fall = 0
             for idx in range(len(actual_list)):
                 ret = 1 if corrects[idx]==prediction[idx] else 0
                 #print('%d, %d, %d'%(corrects[idx], prediction[idx], ret))
-                if ret == 1: count += 1
-            line += '下落2％: %dd: 正答数: %d, 正答率: %.2f (％)\n'%(l, count, 100*count/len(actual_list))
-        with open('%s/probs.txt'%dir_path, 'w') as f: f.write(line)
+                if ret == 1: count_fall += 1
+            line += '%dd_2％, %d, %d, %d, %.2f, %.2f\n'%(l, len(actual_list), count_raise, count_fall, 100*count_raise/len(actual_list), 100*count_fall/len(actual_list))
+        with open('%s/probs.csv'%dir_path, 'w') as f: f.write(line)
