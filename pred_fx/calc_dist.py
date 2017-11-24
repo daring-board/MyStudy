763f893b424b5c031db86a066272405b2752c983
daring-board <mahotox101500@gmail.com>
2017-11-25 03:13:04 +0900

    クラスタリングの誤りを修正。

--- pred_fx/calc_dist.py
+++ pred_fx/calc_dist.py
@@ -8,7 +8,7 @@ class CalcDist():
     Use k-clustering
     '''
     def __init__(self):
-        self.__k = 15
+        self.__k = 20
         self.__span = 60
         self.__input_path = './data/Close/'
 #        self.__nd_list = ['5d', '10d', '15d', '20d']
@@ -36,25 +36,42 @@ class CalcDist():
                 d_mtx[idx+idy*len(stocks)] = self.readDatas(stock1, stock2, noday)
         return d_mtx
 
+    def Find(self, u):
+        '''return Name of set contains vertex u
+        '''
+        return self.__comp[u]
+
+    def Union(self, A, B):
+        ''' return new Set of Union A and B
+        '''
+        S = A.union(B)
+        for item in S: self.__comp[item] = S
+
+    def MakeUnionFind(self, S):
+        self.__comp = {item: set([item]) for item in S}
+
+    def clustersSize(self):
+        clusters = set()
+        for val in self.__comp.values():
+            clusters.add(tuple(val))
+        return len(clusters)
+
     def createClusters(self, stocks, o_list):
-        c_set = []
+        self.MakeUnionFind(stocks)
         count = 0
-        while len(c_set) != self.__k:
+        while self.__k < self.clustersSize():
             item = o_list[count]
             idx, idy = int(item[0]/len(stocks)), int(item[0]%len(stocks))
-            tmp = set([stocks[idx], stocks[idy]])
-            if len(c_set) == 0: c_set.append(tmp)
-            else:
-                flag = True
-                for s in c_set:
-                    if len(s.intersection(tmp)) != 0:
-                        c_set.remove(s)
-                        c_set.append(s.union(tmp))
-                        flag = False
-                        break
-                if flag: c_set.append(tmp)
+            s1 = set([stocks[idx]])
+            s2 = set([stocks[idy]])
+            if count == 0: self.Union(s1, s2)
+            elif self.Find(stocks[idx]) != self.Find(stocks[idy]):
+                self.Union(self.Find(stocks[idx]), self.Find(stocks[idy]))
             count += 1
-        return c_set
+        clusters = set()
+        for val in self.__comp.values():
+            clusters.add(tuple(val))
+        return clusters
 
     def main(self):
         stocks = self.getStocks(self.__input_path)[:60]
@@ -62,7 +79,7 @@ class CalcDist():
             mtx = self.createDistMat(stocks, noday)
             ordered_list = sorted(mtx.items(), key=lambda x: x[1])
             clusters = self.createClusters(stocks, ordered_list)
-            print(clusters)
+            for c in clusters: print(c)
 
 if __name__=='__main__':
     cd = CalcDist()
