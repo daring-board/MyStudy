ad8c29d3e4ee887e0a9457c7bce9e2dca82b445d
daring-board <mahotox101500@gmail.com>
2017-12-03 23:54:12 +0900

    処理を並列化

--- pred_fx/pred_volat_graphs.py
+++ pred_fx/pred_volat_graphs.py
@@ -5,6 +5,7 @@ import os.path
 import numpy as np
 import pandas as pd
 from matplotlib import pylab as plt
+from multiprocessing import Pool
 from fbprophet import Prophet
 
 class PredVolatGraphs():
@@ -12,18 +13,13 @@ class PredVolatGraphs():
     Class PredVolatGraphs
     Predict stock price volatility graphs
     '''
-    def __init__(self):
-        self.__input_path = './data/Close/'
+    def __init__(self, path):
+        self.__input_path = path
         self.__dlen_list = [5, 10, 20, 60]
         self.__rows = 240
         self.__pred_num = 5
         self.extra_num = 1160
         self.__periods = 60
-
-    def getStocks(self, dir_path):
-        dir_list = os.listdir(dir_path)
-        stocks = [item.split('.')[0] for item in dir_list]
-        return stocks
 
     def readDatas(self, path):
         dateparse = lambda dates: pd.datetime.strptime(dates, '%Y/%m/%d')
@@ -40,55 +36,63 @@ class PredVolatGraphs():
         s_val['cap'] = max_vals
         return s_val
 
-    def main(self):
-        stocks = self.getStocks(self.__input_path)[:60]
+    def main(self, stock):
         spans = int(120/self.__pred_num)
-        for stock in stocks:
-            path = self.__input_path+'%s.csv'%stock
-            dir_path = './result/%s'%stock
-            if not os.path.exists(dir_path): os.makedirs(dir_path)
-            s_val = self.readDatas(path)
-            for span in range(spans):
-                start = self.extra_num+self.__pred_num*span-self.__rows
-                end = start+self.__rows
-                train_data = s_val[start: end]
-                model = Prophet(
-                    growth='logistic',
-                    changepoint_prior_scale=0.1,
-                    n_changepoints=50,
-                    yearly_seasonality=True,
-                    weekly_seasonality=True,
-                    daily_seasonality=True
-                )
-                model.fit(train_data)
+        path = self.__input_path+'%s.csv'%stock
+        dir_path = './result/%s'%stock
+        if not os.path.exists(dir_path): os.makedirs(dir_path)
+        s_val = self.readDatas(path)
+        for span in range(spans):
+            start = self.extra_num+self.__pred_num*span-self.__rows
+            end = start+self.__rows
+            train_data = s_val[start: end]
+            model = Prophet(
+                growth='logistic',
+                changepoint_prior_scale=0.1,
+                n_changepoints=50,
+                yearly_seasonality=True,
+                weekly_seasonality=True,
+                daily_seasonality=True
+            )
+            model.fit(train_data)
 
-                future = model.make_future_dataframe(periods=self.__periods)
-                future['cap'] = [train_data['cap'][start] for idx in range(len(future))]
-                forecast = model.predict(future)
-                model.plot(forecast)
-                filename = '%s/output%d.png'%(dir_path, span+1)
-                plt.savefig(filename)
-                plt.close()
-                model.plot_components(forecast)
-                filename = '%s/trend%s.png'%(dir_path, span+1)
-                plt.savefig(filename)
-                plt.close()
-                for pred_date in self.__dlen_list:
-                    if (span*self.__pred_num)%pred_date != 0: continue
-                    with open('%s/result_%dd.csv'%(dir_path, pred_date), 'a') as f:
-                        if span == 0: f.write('date,actual,predict,error,confidence_interval,trend\n')
-                        for idx in range(pred_date):
-                            row_idx = end+idx
-                            f_idx = len(forecast)-self.__periods+idx
-                            line = '%s, %f, %f'%(s_val['ds'][row_idx], s_val['y'][row_idx],forecast['yhat'][f_idx])
-                            line += ', %f, (%f_%f)'%( abs(s_val['y'][row_idx]-forecast['yhat'][f_idx]), forecast['yhat_lower'][f_idx], forecast['yhat_upper'][f_idx])
-                            line += ', %f\n'%forecast['trend'][f_idx]
-                            f.write(line)
-                del forecast
-                del future
-                del model
-                gc.collect()
+            future = model.make_future_dataframe(periods=self.__periods)
+            future['cap'] = [train_data['cap'][start] for idx in range(len(future))]
+            forecast = model.predict(future)
+            model.plot(forecast)
+            filename = '%s/output%d.png'%(dir_path, span+1)
+            plt.savefig(filename)
+            plt.close()
+            model.plot_components(forecast)
+            filename = '%s/trend%s.png'%(dir_path, span+1)
+            plt.savefig(filename)
+            plt.close()
+            for pred_date in self.__dlen_list:
+                if (span*self.__pred_num)%pred_date != 0: continue
+                with open('%s/result_%dd.csv'%(dir_path, pred_date), 'a') as f:
+                    if span == 0: f.write('date,actual,predict,error,confidence_interval,trend\n')
+                    for idx in range(pred_date):
+                        row_idx = end+idx
+                        f_idx = len(forecast)-self.__periods+idx
+                        line = '%s, %f, %f'%(s_val['ds'][row_idx], s_val['y'][row_idx],forecast['yhat'][f_idx])
+                        line += ', %f, (%f_%f)'%( abs(s_val['y'][row_idx]-forecast['yhat'][f_idx]), forecast['yhat_lower'][f_idx], forecast['yhat_upper'][f_idx])
+                        line += ', %f\n'%forecast['trend'][f_idx]
+                        f.write(line)
+            del forecast
+            del future
+            del model
+            gc.collect()
+
+def getStocks(dir_path):
+    dir_list = os.listdir(dir_path)
+    stocks = [item.split('.')[0] for item in dir_list]
+    return stocks
 
 if __name__=='__main__':
-    pvg = PredVolatGraphs()
-    pvg.main()
+    path = './data/Close/'
+    stocks = getStocks(path)[:60]
+    pvg = PredVolatGraphs(path)
+    p = Pool(4)
+    p.map_async(pvg.main, stocks).get(999999)
+    p.close()
+#    for stock in stocks: pvg.main(stock)
