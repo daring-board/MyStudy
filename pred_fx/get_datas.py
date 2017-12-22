4fd14818935dd7a55c16b43b6c7a7b5c037fc354
iwasa-isfactory <iwasa@isfactory.co.jp>
2017-12-22 18:31:42 +0900

    データダウンロードロジックを変更。

--- pred_fx/get_datas.py
+++ pred_fx/get_datas.py
@@ -9,10 +9,19 @@ config.read('./config.ini')
 def createCSV(path, code):
     data = quandl.get('TSE/%s'%code, rows=rows)
     df = pd.DataFrame({
-            'ds': data.index,
+            'ds': data.index.astype('str'),
             'y': data['Close']
         })
     df.index = range(1, int(rows)+1)
+    print(df)
+    datas = pd.read_csv('%s/%s.csv'%(path, code))
+    df2 = pd.DataFrame({
+            'ds': datas['ds'],
+            'y': datas['y']
+        })
+    print(df2)
+    df = pd.concat([df2, df], axis=0)
+    df.index = range(1, 360+1)
     df.to_csv('%s/%s.csv'%(path, code), encoding='utf-8')
 
 if __name__=='__main__':
