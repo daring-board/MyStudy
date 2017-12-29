b81a1af13949334c2ac02dfaf153fd74f7cf2e72
daring-board <mahotox101500@gmail.com>
2017-12-30 03:20:19 +0900

    DPによる売買の最適化。

--- pred_fx/calc_return4.py
+++ pred_fx/calc_return4.py
@@ -7,7 +7,6 @@ from stock_info import StockInfo
 class DynamicPrograming():
 
     def __init__(self, x, val, trend, date):
-        print('construct')
         self.x = x
         self.v = val
         '''
@@ -17,48 +16,65 @@ class DynamicPrograming():
         self.DtoN = {date[idx]: idx for idx in range(len(date))}
         self.NtoD = date
         self.len = len(date)
-        self.act = {}
-        self.opt = [0 for idx in range(self.len+1)]
+        self.act = {self.NtoD[idx]:{self.NtoD[idy]:[] for idy in range(idx+1, self.len)} for idx in range(self.len)}
+        self.opt = {self.NtoD[idx]:{self.NtoD[idy]: 0 for idy in range(idx+1, self.len)} for idx in range(self.len)}
 
     '''
     :buy    : date(string)
     :sell   : date(string)
     '''
     def expectReturn(self, buy, sell):
-        print('expectReturn')
         z = sum(self.trend[self.NtoD[idx]] for idx in range(self.DtoN[buy], self.DtoN[sell]+1))
         term1 = (self.x-self.DtoN[sell]-self.DtoN[buy]+1)
         term1 = 1 - term1 / (self.x-self.len-self.DtoN[buy]+1)
         term2 = 1 - z / (self.DtoN[sell]-self.DtoN[buy]+1)
         term3 = self.v[sell] - self.v[buy]
-        print('%f*%f*%f=%f\n'%(term1, term2, term3, term1*term2*term3))
         return term1*term2*term3
 
-    def commit(self, date):
-        print('commit')
+    def commit(self, yesterday, today, alf):
         price = self.v
-        s_t = sum(price[self.act[key]] if date == self.act[key] else 0 for key in self.act.keys())
+        if alf not in self.opt[yesterday]: return 0
+        s_t = sum(price[today]-price[item[0]] if item[1] == today else 0 for item in self.act[yesterday][alf])
         return s_t
 
-    def maxExpect(self, today):
-        max_val = 0
-        d_tmp = 0
-        for idx in range(self.DtoN[today]+1, self.len):
-            tmp = self.expectReturn(today, self.NtoD[idx])
-            if tmp > max_val:
-                max_val = tmp
-                d_tmp = self.NtoD[idx]
-        self.act[today] = d_tmp
-        return max_val
-
     def main(self):
-        for idx in range(1, self.len+1):
-            today = self.NtoD[idx-1]
-            e_ta = self.maxExpect(today)
-            s_t = self.commit(today)
-            self.opt[idx] = e_ta + s_t + self.opt[idx-1]
-        print(self.opt)
-        print(self.act)
+        today = self.NtoD[0]
+        for idy in range(self.DtoN[today]+1, self.len):
+            alf = self.NtoD[idy]
+            val = self.expectReturn(today, alf)
+            if val > 0:
+                self.opt[today][alf] = val
+                self.act[today][alf].append((today, alf))
+        for idx in range(1, self.len-1):
+#        for idx in range(1, 3):
+            today = self.NtoD[idx]
+            yesterday = self.NtoD[idx-1]
+            tmp, max_exp = [], 0
+            for idy in range(self.DtoN[today]+1, self.len):
+                if self.NtoD[idy] not in self.opt[yesterday]: continue
+                if max_exp < self.opt[yesterday][self.NtoD[idy]]:
+                    max_exp = self.opt[yesterday][self.NtoD[idy]]
+                    tmp = self.act[yesterday][self.NtoD[idy]]
+            for idy in range(self.DtoN[today]+1, self.len):
+                alf = self.NtoD[idy]
+                if alf not in self.opt[yesterday]: continue
+                val = max_exp + self.expectReturn(today, alf) + self.commit(yesterday, today, alf)
+                self.act[today][alf] = tmp.copy()
+                if val > self.opt[yesterday][alf]:
+                    self.opt[today][alf] = val
+                    self.act[today][alf].append((today, alf))
+                else:
+                    self.opt[today][alf] = self.opt[yesterday][alf]
+        # with open('test.csv', 'w') as f:
+        #     f.write('No,')
+        #     for idx in range(1, self.len): f.write('%s,'%self.NtoD[idx])
+        #     f.write('\n')
+        #     for idx in range(0, self.len):
+        #         f.write('%s,'%self.NtoD[idx])
+        #         for idy in range(1, self.len):
+        #             if self.NtoD[idy] not in self.opt[self.NtoD[idx]]: f.write('nan,')
+        #             else: f.write('%f,'%self.opt[self.NtoD[idx]][self.NtoD[idy]])
+        #         f.write('\n')
 
 def getStocks():
     input_path = './data/Close/'
@@ -87,11 +103,42 @@ def readClose(stock):
 if __name__=='__main__':
     stocks = getStocks()
     noday = sys.argv[1]
-    stock = stocks[0]
-    p_price, date = readDatas(stock, noday)
-    p_price = {date[idx]: float(p_price[idx]) for idx in range(len(date))}
-    trend, date = readTrend(stock, noday)
-    trend = {date[idx]: float(trend[idx].strip()) for idx in range(len(date))}
-    diff_trend = {date[idx]: trend[date[idx]]-trend[date[idx-1]] for idx in range(1, len(date))}
-    dp = DynamicPrograming(5, p_price, trend, date)
-    dp.main()
+    for stock in stocks:
+        print(stock)
+        p_price, date = readDatas(stock, noday)
+        p_price = {date[idx]: float(p_price[idx]) for idx in range(len(date))}
+        trend, date = readTrend(stock, noday)
+        trend = {date[idx]: float(trend[idx].strip()) for idx in range(len(date))}
+        diff_trend = {date[idx]: trend[date[idx]]-trend[date[idx-1]] for idx in range(1, len(date))}
+        dp = DynamicPrograming(int(noday[:-1]), p_price, trend, date)
+        dp.main()
+        act = dp.act[date[len(date)-2]][date[len(date)-1]]
+        close = readClose(stock)
+        count = 1
+        with open('result/%s/%s_bs.csv'%(stock, noday), 'w') as f:
+            f.write('No, buy, sell, unit, price\n')
+            buy, sell, pre, num = act[0][0], act[0][1], act[0][1], 0
+            price = 0
+            for item in act:
+                num += 1
+                if pre == item[1] and item != act[-1]: continue
+                if item[1] != 0:
+                    if item == act[-1]: num += 1
+                    if buy == act[0][0]: num -= 1
+                    f.write('%d,%s,%s,%d,%f\n'%(count, buy, sell, num ,num*(close[sell]-close[buy])))
+                    price += num*(close[sell]-close[buy])
+                    count += 1
+                    num = 0
+                    pre = sell = item[1]
+                    buy = item[0]
+            f.write('%f\n'%price)
+            f.write('No, buy, sell, price\n')
+            count, price = 1, 0
+            for item in act:
+                if item[1] != 0:
+                    buy = item[0]
+                    f.write('%d,%s,%s,%f\n'%(count, buy, item[1], close[item[1]]-close[buy]))
+                    price += close[item[1]]-close[buy]
+                    count += 1
+            f.write('%f\n'%price)
+            f.write(str(act)+'\n')
