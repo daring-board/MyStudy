3d3d1005201f6a9149a820271bd691beb6b6018d
daring-board <mahotox101500@gmail.com>
2017-12-29 14:27:26 +0900

    データ読み込みモードを追加

--- pred_fx/get_datas.py
+++ pred_fx/get_datas.py
@@ -6,30 +6,48 @@ import configparser as cp
 config = cp.SafeConfigParser()
 config.read('./config.ini')
 
-def createCSV(path, code):
-    all_rows = int(config.get('config', 'all_rows'))
-    data = quandl.get('TSE/%s'%code, rows=rows)
-    df = pd.DataFrame({
-            'ds': data.index.astype('str'),
-            'y': data['Close']
-        })
-    df.index = range(1, int(rows)+1)
-    print(df)
-    datas = pd.read_csv('%s/%s.csv'%(path, code))
-    df2 = pd.DataFrame({
-            'ds': datas['ds'],
-            'y': datas['y']
-        })
-    df = pd.concat([df2, df], axis=0)
-    df.index = range(1, all_rows+int(rows)+1)
-    df = df[int(rows):]
-    df.index = range(1, all_rows+1)
-    df.to_csv('%s/%s.csv'%(path, code), encoding='utf-8')
+class StockReader():
+
+    def appendCSV(self, path, code):
+        all_rows = int(config.get('config', 'all_rows'))
+        data = quandl.get('TSE/%s'%code, rows=rows)
+        df = pd.DataFrame({
+                'ds': data.index.astype('str'),
+                'y': data['Close']
+            })
+        df.index = range(1, int(rows)+1)
+        print(df)
+        datas = pd.read_csv('%s/%s.csv'%(path, code))
+        df2 = pd.DataFrame({
+                'ds': datas['ds'],
+                'y': datas['y']
+            })
+        df = pd.concat([df2, df], axis=0)
+        df.index = range(1, all_rows+int(rows)+1)
+        df = df[int(rows):]
+        df.index = range(1, all_rows+1)
+        df.to_csv('%s/%s.csv'%(path, code), encoding='utf-8')
+
+    def createCSV(self, path, code):
+        all_rows = int(config.get('config', 'all_rows'))
+        data = quandl.get('TSE/%s'%code, rows=all_rows)
+        df = pd.DataFrame({
+                'ds': data.index.astype('str'),
+                'y': data['Close']
+            })
+        df.index = range(1, int(all_rows)+1)
+        df.to_csv('%s/%s.csv'%(path, code), encoding='utf-8')
 
 if __name__=='__main__':
     path = sys.argv[1] # 保存先を指定。
+    mode = sys.argv[2] # データの読み込みモード。
     stocks = config.get('config', 'codes').strip().split(',')
     rows = config.get('config', 'rows')
     quandl.ApiConfig.api_key = config.get('config', 'api_key')
-    createCSV('data', 1592)
-    for stock in stocks: createCSV(path, stock)
+    reader = StockReader()
+    if mode == 'append':
+       reader.createCSV('data', 1592)
+       for stock in stocks: reader.createCSV(path, stock)
+   else:
+       stock = sys.argv[3]
+       reader.createCSV(path, stock)
